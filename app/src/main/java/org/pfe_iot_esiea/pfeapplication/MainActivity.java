429c200c3607316ced9d05918e83030ff2de68e6
Vinc91 <hike91@hotmail.fr>
2017-12-28 19:55:07 +0100

    2nd Commit

--- app/src/main/java/org/pfe_iot_esiea/pfeapplication/MainActivity.java
+++ app/src/main/java/org/pfe_iot_esiea/pfeapplication/MainActivity.java
@@ -1,11 +1,20 @@
 package org.pfe_iot_esiea.pfeapplication;
 
+import android.bluetooth.BluetoothAdapter;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.kontakt.sdk.android.ble.configuration.ActivityCheckConfiguration;
+import com.kontakt.sdk.android.ble.configuration.ForceScanConfiguration;
+import com.kontakt.sdk.android.ble.configuration.ScanMode;
+import com.kontakt.sdk.android.ble.configuration.ScanPeriod;
 import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
+import com.kontakt.sdk.android.ble.device.EddystoneDevice;
 import com.kontakt.sdk.android.ble.manager.ProximityManager;
 import com.kontakt.sdk.android.ble.manager.ProximityManagerFactory;
 import com.kontakt.sdk.android.ble.manager.listeners.EddystoneListener;
@@ -18,20 +27,60 @@ import com.kontakt.sdk.android.common.profile.IBeaconRegion;
 import com.kontakt.sdk.android.common.profile.IEddystoneDevice;
 import com.kontakt.sdk.android.common.profile.IEddystoneNamespace;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
 
     private ProximityManager proximityManager;
     private boolean Beacon1recognized = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         KontaktSDK.initialize("NOpUjpWwpCckCePHQnjZngXENPFcmZMj");
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+        Button monbutton = (Button) findViewById(R.id.button_scan);
+        monbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                proximityManager.restartScanning();
+            }
+        });
+
+        if(mBluetoothAdapter == null){
+
+        }else{
+            if (!mBluetoothAdapter.isEnabled()) {
+                // Bluetooth is not enable :)
+                mBluetoothAdapter.enable();
+            }
+        }
         proximityManager = ProximityManagerFactory.create(this);
+        configureProximityManager();
         proximityManager.setIBeaconListener(createIBeaconListener());
         proximityManager.setEddystoneListener(createEddystoneListener());
+
+
     }
+
+    private void configureProximityManager() {
+        proximityManager.configuration()
+                .scanMode(ScanMode.LOW_LATENCY)
+                .scanPeriod(ScanPeriod.create(TimeUnit.SECONDS.toMillis(10), TimeUnit.SECONDS.toMillis(20)))
+                .activityCheckConfiguration(ActivityCheckConfiguration.DEFAULT)
+                .forceScanConfiguration(ForceScanConfiguration.MINIMAL);
+
+    }
+
+
 
     @Override
     protected void onStart() {
@@ -58,19 +107,28 @@ public class MainActivity extends AppCompatActivity {
             public void onServiceReady() {
                 proximityManager.startScanning();
             }
+
         });
     }
+
+
 
     private IBeaconListener createIBeaconListener() {
         return new SimpleIBeaconListener() {
             @Override
             public void onIBeaconDiscovered(IBeaconDevice ibeacon, IBeaconRegion region) {
                 Log.i("Sample", "IBeacon discovered: " + ibeacon.toString());
+
             }
         };
     }
 
+
+
     private EddystoneListener createEddystoneListener() {
+
+
+
         return new SimpleEddystoneListener() {
             @Override
             public void onEddystoneDiscovered(IEddystoneDevice eddystone, IEddystoneNamespace namespace) {
@@ -79,18 +137,27 @@ public class MainActivity extends AppCompatActivity {
                 TextView monTexte = (TextView)findViewById(R.id.beacon1);
                 if(!Beacon1recognized) {
                     if(eddystone.getInstanceId().equals("6e4b50795750"))
-                        monTexte.setText("Beacon JBqt detected");
+                        monTexte.setText("Beacon JBqt detected " + eddystone.getDistance());
                     else if (eddystone.getInstanceId().equals("756b72646934"))
-                        monTexte.setText("Beacon q5nW  detected");
+                        monTexte.setText("Beacon q5nW  detected "  + eddystone.getDistance());
                     Beacon1recognized = true;
                 }
                 else{
                     monTexte = (TextView)findViewById(R.id.beacon2);
                     if(eddystone.getInstanceId().equals("6e4b50795750"))
-                        monTexte.setText("Beacon JBqt detected");
+                        monTexte.setText("Beacon JBqt detected "  + eddystone.getDistance());
                     else if (eddystone.getInstanceId().equals("756b72646934"))
-                        monTexte.setText("Beacon q5nW  detected");
+                        monTexte.setText("Beacon q5nW  detected " + eddystone.getDistance());
+                    Beacon1recognized = false;
                 }
+
+
+
+            }
+
+            @Override
+            public void onEddystonesUpdated(List<IEddystoneDevice> eddystones, IEddystoneNamespace namespace) {
+                super.onEddystonesUpdated(eddystones, namespace);
 
             }
         };
